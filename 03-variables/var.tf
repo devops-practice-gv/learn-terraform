e6692705853d48de9013a2271edaa6393238151a
Gaurav <gauravenigma@gmail.com>
2024-03-18 04:53:26 +0000

    Output Terraform

--- 03-variables/var.tf
+++ 03-variables/var.tf
@@ -7,7 +7,31 @@ variable "x" {
 # Declare a variable WITHOUT a value
 variable "y" {}
 
+# Declare a list variable
+variable "y_list" {
+  default = [ 1,2,3]
+}
+
+# Declare a map variable
+variable "z" {
+  default = {
+    name = gv
+    age = 40
+    height = 176
+  }
+}
+
 # print the variable
 output "y" {
   value = var.y
+}
+
+# print the variable
+output "y_list" {
+  value = var.y_list
+}
+
+# print the variable
+output "z" {
+  value = var.z
 }
