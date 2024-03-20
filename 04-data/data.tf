cf7774e63054a50c7395ee47a1d6bfd85cccb8c2
Gaurav <gauravenigma@gmail.com>
2024-03-20 03:23:58 +0000

    Output Terraform

--- 04-data/data.tf
+++ 04-data/data.tf
@@ -5,3 +5,9 @@ data "aws_security_group" "expense_app_security_group" {
 output "ID_of_Sec_Grp" {
   value = data.aws_security_group.expense_app_security_group.id
 }
+
+# Data are of three types
+
+#String >> needs double quotes
+#number no quote
+#boolean no quotes
