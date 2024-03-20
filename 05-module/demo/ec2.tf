d5adb98e7618e82f010945222bcbe253f642309a
Gaurav <gauravenigma@gmail.com>
2024-03-20 00:05:17 +0000

    Output Terraform

--- 05-module/demo/ec2.tf
+++ 05-module/demo/ec2.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "Test1" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.small"
+  tags = {
+    name = var.Name
+  }
 }
+
+variable "Name" {}
--- 05-module/main.tf
+++ 05-module/main.tf
@@ -1,7 +1,9 @@
 module "demo-1" {
   source = "./demo"
+  Name = "Demo-1"
 }
 
 module "demo-2" {
   source = "./demo"
+  Name = "Demo-2"
 }
