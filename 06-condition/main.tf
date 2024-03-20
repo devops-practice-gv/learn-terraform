5094e5d1617df11ac6f3e4852219fdb7f822fe18
Gaurav <gauravenigma@gmail.com>
2024-03-20 00:37:56 +0000

    Output Terraform

---
+++ 06-condition/main.tf
@@ -0,0 +1,10 @@
+# Conditions in terraform is all about  choosing the value
+#cond? true-val  : fal-var
+resource "aws_instance" "Test1" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = var.instance_type == "t2" ? "t3.micro" : var.instance_type
+}
+
+# if the user give a option then use
+
+variable "instance_type" {}
