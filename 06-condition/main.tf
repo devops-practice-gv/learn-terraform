527f3404d824cdcbbf90be596a777816fdc997d3
Gaurav <gauravenigma@gmail.com>
2024-03-20 02:17:38 +0000

    Output Terraform

--- 06-condition/main.tf
+++ 06-condition/main.tf
@@ -2,7 +2,7 @@
 #cond? true-val  : fal-var
 resource "aws_instance" "Test1" {
   ami           = "ami-05f020f5935e52dc4"
-  instance_type = var.instance_type == "t2.*" ? "t3.micro" : var.instance_type
+  instance_type = var.instance_type == regex("^t2.*") ? "t3.micro" : var.instance_type
 }
 
 # if the user give a option then use
