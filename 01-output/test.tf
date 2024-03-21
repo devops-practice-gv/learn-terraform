516bc7308688d3fabcd6242b0c199f7934bcd383
Gaurav <gauravenigma@gmail.com>
2024-03-21 17:16:11 +0000

    Multi State Terraform

--- 01-output/test.tf
+++ 01-output/test.tf
@@ -1,3 +1,7 @@
 output "test" {
   value = "Hello world"
 }
+
+#output "subnet_id" {
+#  value = aws_instance.test.subnet_id
+#}
--- 02-resource/ec2.tf
+++ 02-resource/ec2.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "Test1" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.small"
+#  subnet_id = "subnet-03974e21a954e3e4b"
 }
+
